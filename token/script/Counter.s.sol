60a2632949bdc8880e6f61d81b907b872f04bf05
Joseph Schiarizzi <9449596+cupOJoseph@users.noreply.github.com>
2024-11-24 00:38:56 -0500

    init

--- token/script/Counter.s.sol
+++ token/script/Counter.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {Token} from "../src/Token.sol";
 
 contract CounterScript is Script {
     Counter public counter;
@@ -12,7 +12,7 @@ contract CounterScript is Script {
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        counter = new Token();
 
         vm.stopBroadcast();
     }
